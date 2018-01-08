3bbc066dce9e03a49d0b0784f38d5d20331ab40e
DimaMukhin <dima9348@gmail.com>
2018-01-08 11:30:00 -0600

    updated server to remote mLab

--- server/api.js
+++ server/api.js
@@ -5,13 +5,15 @@ const mongoose = require('mongoose');
 /**
  * Connecting to database
  */
-// const db = "mongodb://localhost:27017/local";
-// mongoose.Promise = require('bluebird');
-// mongoose.connect(db, function(err){
-//     if(err){
-//         console.error("Error! " + err);
-//     }
-// });
+const db = "mongodb://admin:admin@ds245277.mlab.com:45277/heroku_1r1pwztj";
+mongoose.Promise = require('bluebird');
+mongoose.connect(db, function(err){
+    if(err){
+        console.error("Error! " + err);
+    }
+});
+
+// #4Ne3^k91%S1
 
 /**
  * API Health status
