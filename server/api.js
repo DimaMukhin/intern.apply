76f3fa6d070b4a0fbf76f7166ca160cbe6f1643e
DimaMukhin <dima9348@gmail.com>
2018-01-08 09:57:46 -0600

    removed local db connection

--- server/api.js
+++ server/api.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 /**
  * Connecting to database
  */
-const db = "mongodb://localhost:27017/local";
-mongoose.Promise = require('bluebird');
-mongoose.connect(db, function(err){
-    if(err){
-        console.error("Error! " + err);
-    }
-});
+// const db = "mongodb://localhost:27017/local";
+// mongoose.Promise = require('bluebird');
+// mongoose.connect(db, function(err){
+//     if(err){
+//         console.error("Error! " + err);
+//     }
+// });
 
 /**
  * API Health status
@@ -21,8 +21,7 @@ router.get('/', (req, res) => {
 });
 
 // setting up all api routes
-require('./routes/student.route')(router);
-require('./routes/course.route')(router);
+require('./routes/employer.route')(router);
 
 // development playground
 // require('./playground');
