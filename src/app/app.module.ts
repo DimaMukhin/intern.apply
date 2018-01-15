7fe7a0154eb43206176aa706dc8543f0a3844d5c
DimaMukhin <dima9348@gmail.com>
2018-01-14 21:28:31 -0600

    implemented ng internAPI service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InternApiService } from './shared/services/intern-api/intern-api.service';
 
 
 @NgModule({
@@ -18,10 +20,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AppRoutingModule,
     BsDropdownModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    InternApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/services/intern-api/intern-api.service.ts
+++ src/app/shared/services/intern-api/intern-api.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+const BASE_URL = 'http://localhost:3000';
 
 @Injectable()
 export class InternApiService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  /**
+   * get all the jobs from internAPI
+   * @returns Observable holding list of jobs
+   */
+  public getAllJobs(): Observable<any> {
+    return this.http.get(BASE_URL + '/api/job').map((res: Response) => {
+      return res.json();
+    });
+  }
 
 }
