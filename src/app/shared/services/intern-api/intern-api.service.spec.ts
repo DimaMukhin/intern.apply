bddff402a537764569fb6456ef05ec7049478fb5
Shai <fridbois@myumanitoba.ca>
2018-01-26 14:04:21 -0600

    fixed missing files - everything compiles

---
+++ src/app/shared/services/intern-api/intern-api.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+
+import { InternApiService } from './intern-api.service';
+
+describe('InternApiService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [InternApiService],
+            imports: [ HttpModule ]
+        });
+    });
+
+    it('should be created', inject([InternApiService], (service: InternApiService) => {
+        expect(service).toBeTruthy();
+    }));
+});
