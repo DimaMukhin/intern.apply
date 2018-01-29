80bccdcc24080c05aa99e425b1d2334a9b1f7852
Shai <fridbois@myumanitoba.ca>
2018-01-29 14:04:12 -0600

    added http requst to api

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 // import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+    public user = {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        email: '',
+        emailConfirm: ''
+    };
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/shared/services/intern-api/intern-api.service.ts
+++ src/app/shared/services/intern-api/intern-api.service.ts
@@ -1,25 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-import { environment } from '../../../../environments/environment';
+import {environment} from '../../../../environments/environment';
 
 const BASE_URL = environment.host;
 
 @Injectable()
 export class InternApiService {
 
-  constructor(private http: Http) { }
+    constructor(private http: Http) {
+    }
 
-  /**
-   * get all the jobs from internAPI
-   * @returns Observable holding list of jobs
-   */
-  public getAllJobs(): Observable<any> {
-    return this.http.get(BASE_URL + '/api/job').map((res: Response) => {
-      return res.json();
-    });
-  }
+    /**
+     * get all the jobs from internAPI
+     * @returns Observable holding list of jobs
+     */
+    public getAllJobs(): Observable<any> {
+        return this.http.get(BASE_URL + '/api/job').map((res: Response) => {
+            return res.json();
+        });
+    }
+
+    /*
+    * register a new user using registration API
+    * @returns a Observable for confirmation
+    */
+    public register(user: Object): Observable<any> {
+        return this.http.post(BASE_URL + '/reg', user).map((res: Response) => {
+            return res.json();
+        });
+    }
 
 }
