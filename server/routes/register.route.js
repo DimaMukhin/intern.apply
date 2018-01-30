440228dff8152ddf6010161c0f3b9eba0bc9766d
Shai <fridbois@myumanitoba.ca>
2018-01-29 18:46:46 -0600

    added comments, deleted break lines

--- server/routes/register.route.js
+++ server/routes/register.route.js
@@ -1,21 +1,21 @@
 const db = require('../database/db.service');
 
 module.exports = (router) => {
-
     /**
      * add a user to the database
      */
     router.post('/reg', (req, res) => {
+        //create user object with post parameters
         const user = {
             username: res.body.username,
             password: res.body.password,
             email: res.body.email
         };
 
+        // pass user to db
         db.addUser(user, (err, response) => {
             if (err) res.status(400).send(err);
             else res.send(response);
         });
     });
-
 };
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,27 +7,38 @@ import {RegisterApiService} from '../shared/services/register-api/register-api.s
     styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-    public user = {
-        username: undefined,
-        password: undefined,
-        passwordConfirm: undefined,
-        email: undefined,
-        emailConfirm: undefined
-    };
-    public isSubmitted = false;
-    public response: string;
+    public user: object;    // user object
+    public isSubmitted: boolean; // the status of the form submission
+    public response: string;    // the response we get once the form is submitted
 
     constructor(private registerAPI: RegisterApiService) {
     }
 
+    /*
+     * initialization of properties
+     */
     ngOnInit() {
+        this.user = {
+            username: undefined,
+            password: undefined,
+            passwordConfirm: undefined,
+            email: undefined,
+            emailConfirm: undefined
+        };
+        this.isSubmitted = false;
     }
 
+   /*
+    * when the user submits the form
+    */
     onSubmit() {
         this.register();
         this.isSubmitted = true;
     }
 
+    /*
+    * creates an API call using the register service
+    */
     private register(): void {
         this.registerAPI.register(this.user).subscribe((res) => {
             this.response = 'You were successfully registered';
