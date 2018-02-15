df8cc65d4e065394a8badb7c1bf50d94da8c2450
DimaMukhin <dima9348@gmail.com>
2018-02-14 21:32:13 -0600

    implemented job-comment back-end tests

--- server/test/database/db.service.test.js
+++ server/test/database/db.service.test.js
@@ -8,18 +8,24 @@ describe('db.service.js', () => {
 
     describe('ContactMessage', () => {
 
-        beforeEach(() => {
-            db.conn.query('DROP TABLE IF EXISTS contactMessage', (err, res) => { });
-            db.conn.query(`CREATE TABLE contactMessage (
-                id INT NOT NULL AUTO_INCREMENT,
-                email VARCHAR(45) NOT NULL,
-                title VARCHAR(45) NOT NULL,
-                message VARCHAR(300) NOT NULL,
-                PRIMARY KEY (id))`);
-            db.conn.query(`INSERT INTO contactMessage (id, email, title, message) VALUES 
-                (1, 'dima@gmail.com', 'test title', 'test body'),
-                (2, 'ben@gmail.com', 'second title', 'second body'),
-                (3, 'what@is.this', 'third title', 'third body')`);
+        beforeEach((done) => {
+            db.conn.query('DROP TABLE contactMessage', (err, res) => {
+                db.conn.query(`CREATE TABLE contactMessage (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    email VARCHAR(45) NOT NULL,
+                    title VARCHAR(45) NOT NULL,
+                    message VARCHAR(300) NOT NULL,
+                    PRIMARY KEY (id))`
+                , (err, res) => {
+                    db.conn.query(`INSERT INTO contactMessage (id, email, title, message) VALUES 
+                        (1, 'dima@gmail.com', 'test title', 'test body'),
+                        (2, 'ben@gmail.com', 'second title', 'second body'),
+                        (3, 'what@is.this', 'third title', 'third body')`
+                    , (err, res) => {
+                        done();
+                    });
+                });
+            });
         });
 
         describe('getAllContactMessages', () => {
@@ -73,20 +79,25 @@ describe('db.service.js', () => {
 
     describe('GetJobs', () => {
 
-        beforeEach(() => {
-            db.conn.query('DROP TABLE IF EXISTS job', (err, res) => { });
-            db.conn.query(`CREATE TABLE job (
-                id INT NOT NULL AUTO_INCREMENT,
-                organization VARCHAR(45) NOT NULL,
-                title VARCHAR(100) NOT NULL,
-                location VARCHAR(45),
-                description VARCHAR(2000),
-                PRIMARY KEY (id))`
-            );
-            db.conn.query(`INSERT INTO job (id, organization, title, location) VALUES 
-                (1, 'Facebook', 'test title', 'winnipeg'),
-                (2, 'google', 'second title', 'vancouver'),
-                (3, 'CityOFWinnipeg', 'third title', 'location')`);
+        beforeEach((done) => {
+            db.conn.query('DROP TABLE job', (err, res) => { 
+                db.conn.query(`CREATE TABLE job (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    organization VARCHAR(45) NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    location VARCHAR(45),
+                    description VARCHAR(2000),
+                    PRIMARY KEY (id))`
+                , (err, res) => {
+                    db.conn.query(`INSERT INTO job (id, organization, title, location) VALUES 
+                    (1, 'Facebook', 'test title', 'winnipeg'),
+                    (2, 'google', 'second title', 'vancouver'),
+                    (3, 'CityOFWinnipeg', 'third title', 'location')`
+                    , (err, res) => {
+                        done();
+                    });
+                });
+            });
         });
 
         describe('getJob', () => {
@@ -133,20 +144,26 @@ describe('db.service.js', () => {
 
     describe('addJob', () => {
 
-        beforeEach(() => {
-            db.conn.query('DROP TABLE job', (err, res) => { });
-            db.conn.query(`CREATE TABLE job (
-                id INT NOT NULL AUTO_INCREMENT,
-                organization VARCHAR(45) NOT NULL,
-                title VARCHAR(100) NOT NULL,
-                location VARCHAR(45) NOT NULL,
-                description VARCHAR(2000) NOT NULL,
-                PRIMARY KEY (id))`);
-            db.conn.query(`INSERT INTO job (id, organization, title, location, description) VALUES 
-                (1, 'Test Org', 'test title', '123 test st', 'test description'),
-                (2, 'Electronic Test', 'second title', '456 test avenue', 'this is a description for a test'),
-                (3, 'The Test Mafia', 'second title', '456 test avenue', 'this is a long long long long long long long long long long long long long description'),
-                (4, 'Together We Test', 'fourth title', '789 test blvd', 'No description')`);
+        beforeEach((done) => {
+            db.conn.query('DROP TABLE job', (err, res) => { 
+                db.conn.query(`CREATE TABLE job (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    organization VARCHAR(45) NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    location VARCHAR(45) NOT NULL,
+                    description VARCHAR(2000) NOT NULL,
+                    PRIMARY KEY (id))`,
+                (err, res) => {
+                    db.conn.query(`INSERT INTO job (id, organization, title, location, description) VALUES 
+                        (1, 'Test Org', 'test title', '123 test st', 'test description'),
+                        (2, 'Electronic Test', 'second title', '456 test avenue', 'this is a description for a test'),
+                        (3, 'The Test Mafia', 'second title', '456 test avenue', 'this is a long long long long long long long long long long long long long description'),
+                        (4, 'Together We Test', 'fourth title', '789 test blvd', 'No description')`,
+                    (err, res) => {
+                        done();
+                    });
+                });
+            });
         });
 
         it('it should add the new job', (done) => {
@@ -243,6 +260,138 @@ describe('db.service.js', () => {
             db.getAllJobs((err, res, fields) => {
                 expect(res).to.have.lengthOf(4);
                 done();
+            });
+        });
+    });
+
+    describe('comments', () => {
+
+        beforeEach((done) => {
+            db.conn.query('DROP TABLE job', (err, res, fields) => { 
+                db.conn.query(`CREATE TABLE job (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    organization VARCHAR(45) NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    location VARCHAR(45),
+                    description VARCHAR(2000),
+                    PRIMARY KEY (id))`,
+                (err, res) => {
+                    db.conn.query(`INSERT INTO job (id, organization, title, location) VALUES 
+                        (1, 'Facebook', 'test title', 'winnipeg'),
+                        (2, 'google', 'second title', 'vancouver'),
+                        (3, 'CityOFWinnipeg', 'third title', 'location')`,
+                    (err, res) => {
+                        db.conn.query('DROP TABLE comment', (err, res) => {
+                            db.conn.query(`CREATE TABLE comment (
+                                id INT NOT NULL AUTO_INCREMENT,
+                                jobID INT NOT NULL,
+                                message VARCHAR(300) NOT NULL,
+                                author VARCHAR(45) NOT NULL,
+                                ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                PRIMARY KEY (id),
+                                FOREIGN KEY (jobID) REFERENCES job (id))`,
+                            (err, res) => {
+                                db.conn.query(`INSERT INTO comment (id, jobID, message, author) VALUES 
+                                    (1, 1, 'this is a nice comment body', 'dima'),
+                                    (2, 1, 'another comment for the same job', 'ben'),
+                                    (3, 2, 'this last comment is for job 2', 'rick')`,
+                                (err, res) => {
+                                    done();
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+
+        afterEach((done) => {
+            db.conn.query('DROP TABLE comment', (err, res) => {
+                db.conn.query('DROP TABLE job', (err, res) => {
+                    done();
+                });
+            });
+        });
+
+        describe('getAllComments', () => {
+            it('should return 3 comments', (done) => {
+                db.getAllComments((err, res, fields) => {
+                    expect(res).to.have.lengthOf(3);
+                    done();
+                });
+            });
+
+            it('should return the correct first comment', (done) => {
+                db.getAllComments((err, res, fields) => {
+                    let firstRecord = res[0];
+                    expect(firstRecord.id).to.equal(1);
+                    expect(firstRecord.jobID).to.equal(1);
+                    expect(firstRecord.message).to.equal('this is a nice comment body');
+                    expect(firstRecord.author).to.equal('dima');
+                    done();
+                });
+            });
+        });
+
+        describe('getAllCommentsOfJob', () => {
+            it('should return all the comments of job 1', (done) => {
+                db.getAllCommentsOfJob(1, (err, res, fields) => {
+                    expect(res).to.have.lengthOf(2);
+
+                    let comment1 = res[0];
+                    expect(comment1.message).to.equal('this is a nice comment body');
+                    expect(comment1.author).to.equal('dima');
+
+                    let comment2 = res[1];
+                    expect(comment2.message).to.equal('another comment for the same job');
+                    expect(comment2.author).to.equal('ben');
+
+                    done();
+                });
+            });
+
+            it('should return no comments for job 3', (done) => {
+                db.getAllCommentsOfJob(3, (err, res, fields) => {
+                    expect(res).to.have.lengthOf(0);
+                    done();
+                });
+            });
+        });
+
+        describe('addNewComment', () => {
+            it('should add a new valid comment', (done) => {
+                db.addNewComment({jobID: 2, message: 'test message', author: 'dima'}, (err, res, fields) => {
+                    expect(err).to.be.null;
+                    done();
+                });
+            });
+
+            it('should not add a valid comment to an unexisting job', (done) => {
+                db.addNewComment({jobID: 4, message: 'should fail', author: 'ddd'}, (err, res, fields) => {
+                    expect(err).to.exist;
+                    done();
+                });
+            });
+
+            it('should not add a comment with invalid message body', (done) => {
+                db.addNewComment({jobID: 1, message: undefined, author: 'ddd'}, (err, res, fields) => {
+                    expect(err).to.exist;
+                    done();
+                });
+            });
+
+            it('should not add a comment with invalid author name', (done) => {
+                db.addNewComment({jobID: 1, message: 'should fail', author: undefined}, (err, res, fields) => {
+                    expect(err).to.exist;
+                    done();
+                });
+            });
+
+            it('should not add a comment with an existing id', (done) => {
+                db.addNewComment({id: 1, jobID: 1, message: 'should fail', author: 'ddd'}, (err, res, fields) => {
+                    expect(err).to.exist;
+                    done();
+                });
             });
         });
     });
--- server/test/services/validation.service.test.js
+++ server/test/services/validation.service.test.js
@@ -167,4 +167,75 @@ describe('validation.service.js', () => {
             expect(validation.validateContactMessageBody(emptyDesc)).to.be.false;
         });
     });
+
+    describe('validateJobID', () => {
+        it('should return true for a valid job id', () => {
+            let validJobID = 1;
+            expect(validation.validateJobID(validJobID)).to.be.true;
+        });
+
+        it('should return false for a job id that is not a number', () => {
+            let invalidJobID = 'this is not a number';
+            expect(validation.validateJobID(invalidJobID)).to.be.false;
+        });
+
+        it('should return false for a job id that is undefined', () => {
+            let invalidJobID = undefined;
+            expect(validation.validateJobID(invalidJobID)).to.be.false;
+        });
+
+        it('should return false for a job id that is a floating number', () => {
+            let invalidJobID = 4.4;
+            expect(validation.validateJobID(invalidJobID)).to.be.false;
+        });
+
+        it('should return false for a job id that is negative', () => {
+            let invalidJobID = -5;
+            expect(validation.validateJobID(invalidJobID)).to.be.false;
+        });
+    });
+
+    describe('validateCommentMessage', () => {
+        it('should return true for a valid comment message', () => {
+            let validCommentMessage = 'test this message';
+            expect(validation.validateCommentMessage(validCommentMessage)).to.be.true;
+        });
+
+        it('should return false for a comment message that is too short', () => {
+            let invalidCommentMessage = '';
+            expect(validation.validateCommentMessage(invalidCommentMessage)).to.be.false;
+        });
+
+        it('should return false for a comment message that is too long', () => {
+            let invalidCommentMessage = 'too longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo long';
+            expect(validation.validateCommentMessage(invalidCommentMessage)).to.be.false;
+        });
+
+        it('should return false for a comment message that is just empty spaces', () => {
+            let invalidCommentMessage = '          ';
+            expect(validation.validateCommentMessage(invalidCommentMessage)).to.be.false;
+        });
+    });
+
+    describe('validateCommentAuthor', () => {
+        it('should return true for a valid author name', () => {
+            let validAuthorName = 'Dimba';
+            expect(validation.validateCommentAuthor(validAuthorName)).to.be.true;
+        });
+
+        it('should return false for an author name that is too short', () => {
+            let invalidAuthorName = '';
+            expect(validation.validateCommentAuthor(invalidAuthorName)).to.be.false;
+        });
+
+        it('should return false for an author name that is too long', () => {
+            let invalidAuthorName = 'too longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo longtoo long';
+            expect(validation.validateCommentAuthor(invalidAuthorName)).to.be.false;
+        });
+
+        it('should return false for an author name that is just empty spaces', () => {
+            let invalidAuthorName = '          ';
+            expect(validation.validateCommentAuthor(invalidAuthorName)).to.be.false;
+        });
+    });
 });
