076922aef85cb575fcf6932d3e9a7e28d739a8a6
Shai <fridbois@myumanitoba.ca>
2018-02-27 02:08:49 -0600

    implemented angular tests

--- src/app/view-job/job-rating/job-rating.component.spec.ts
+++ src/app/view-job/job-rating/job-rating.component.spec.ts
@@ -1,32 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { JobRatingComponent } from './job-rating.component';
-import { InternApiService } from '../../shared/services/intern-api/intern-api.service';
+import {JobRatingComponent} from './job-rating.component';
+import {InternApiService} from '../../shared/services/intern-api/intern-api.service';
 
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RatingModule } from 'ngx-bootstrap/rating';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RatingModule} from 'ngx-bootstrap/rating';
+import {Observable} from 'rxjs/Observable';
+import {By} from '@angular/platform-browser';
 
 describe('JobRatingComponent', () => {
-  let component: JobRatingComponent;
-  let fixture: ComponentFixture<JobRatingComponent>;
+    let component: JobRatingComponent;
+    let fixture: ComponentFixture<JobRatingComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, FormsModule, HttpModule, RatingModule],
-      declarations: [ JobRatingComponent ],
-      providers: [InternApiService]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule, FormsModule, HttpModule, RatingModule],
+            declarations: [JobRatingComponent],
+            providers: [InternApiService]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JobRatingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JobRatingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get a job rating', async(() => {
+        const service = TestBed.get(InternApiService);
+
+        spyOn(service, 'getJobRating').and.returnValue(Observable.of([{
+            jobID: 1, score: '5.00', votes: 1
+        }]));
+
+        component.jobID = 1;
+        component.getRating();
+        fixture.detectChanges();
+
+        expect(component.score).toBe(5);
+        expect(component.votes).toBe(1);
+    }));
 });
