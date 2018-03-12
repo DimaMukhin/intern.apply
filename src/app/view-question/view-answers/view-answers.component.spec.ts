097146ca402bbade1050b18b39375aee80817b65
Shai <fridbois@myumanitoba.ca>
2018-03-11 20:32:05 -0500

    implemented tests for UI

--- src/app/view-question/view-answers/view-answers.component.spec.ts
+++ src/app/view-question/view-answers/view-answers.component.spec.ts
@@ -48,9 +48,9 @@ describe('ViewAnswersComponent', () => {
     it('should not get any answers for an invalid question id', async(() => {
         const service = TestBed.get(InternApiService);
         spyOn(service, 'getAnswers').and.returnValue(Observable.of([
-            {author: 'dima', message: 'test'},
-            {author: 'ben', message: 'test2'},
-            {author: 'cyka', message: 'blyat'}
+            {author: 'dima', body: 'test'},
+            {author: 'ben', body: 'test2'},
+            {author: 'cyka', body: 'blyat'}
         ]));
 
         component.questionId = 0;
@@ -63,32 +63,48 @@ describe('ViewAnswersComponent', () => {
     it('shoul display all answers for a valid question id', async(() => {
         const service = TestBed.get(InternApiService);
         spyOn(service, 'getAnswers').and.returnValue(Observable.of([
-            {author: 'dima', message: 'test'},
-            {author: 'ben', message: 'test2'},
-            {author: 'cyka', message: 'blyat'}
+            {author: 'dima', body: 'test'},
+            {author: 'ben', body: 'test2'},
+            {author: 'cyka', body: 'blyat'}
         ]));
 
         component.questionId = 1;
         component.getAnswers();
         fixture.detectChanges();
 
-        let deAuthor = fixture.debugElement.queryAll(By.css('.cm-author'));
-        let elAuthor1: HTMLElement = deAuthor[0].nativeElement;
-        let elAuthor2: HTMLElement = deAuthor[1].nativeElement;
-        let elAuthor3: HTMLElement = deAuthor[2].nativeElement;
-        let deMessage = fixture.debugElement.queryAll(By.css('.cm-message'));
-        let elMessage1: HTMLElement = deMessage[0].nativeElement;
-        let elMessage2: HTMLElement = deMessage[1].nativeElement;
-        let elMessage3: HTMLElement = deMessage[2].nativeElement;
+        const deAuthor = fixture.debugElement.queryAll(By.css('.answer-author'));
+        const elAuthor1: HTMLElement = deAuthor[0].nativeElement;
+        const elAuthor2: HTMLElement = deAuthor[1].nativeElement;
+        const elAuthor3: HTMLElement = deAuthor[2].nativeElement;
+        const deMessage = fixture.debugElement.queryAll(By.css('.answer-body'));
+        const elMessage1: HTMLElement = deMessage[0].nativeElement;
+        const elMessage2: HTMLElement = deMessage[1].nativeElement;
+        const elMessage3: HTMLElement = deMessage[2].nativeElement;
 
         expect(deAuthor.length).toBe(3);
         expect(deMessage.length).toBe(3);
-        expect(elAuthor1.innerText).toBe('dima');
-        expect(elAuthor2.innerText).toBe('ben');
-        expect(elAuthor3.innerText).toBe('cyka');
+        expect(elAuthor1.innerText).toBe('By dima at');
+        expect(elAuthor2.innerText).toBe('By ben at');
+        expect(elAuthor3.innerText).toBe('By cyka at');
         expect(elMessage1.innerText).toBe('test');
         expect(elMessage2.innerText).toBe('test2');
         expect(elMessage3.innerText).toBe('blyat');
-        expect(component.comments.length).toBe(3);
+        expect(component.answers.length).toBe(3);
+    }));
+
+    it('shoul not display any answers for an invalid question id', async(() => {
+        const service = TestBed.get(InternApiService);
+        spyOn(service, 'getAnswers').and.returnValue(Observable.of([]));
+
+        component.questionId = 0;
+        component.getAnswers();
+        fixture.detectChanges();
+
+        const deAuthor = fixture.debugElement.queryAll(By.css('.cm-author'));
+        const deMessage = fixture.debugElement.queryAll(By.css('.cm-message'));
+
+        expect(deAuthor.length).toBe(0);
+        expect(deMessage.length).toBe(0);
+        expect(component.answers.length).toBe(0);
     }));
 });
